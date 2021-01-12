69a8a8faa9cb3db9146833f8d90c3bf439ad59ed
Kasper <kasperwissendorf2011@gmail.com>
2021-01-12 15:17:08 +0100

    Update municipalityPositive.cs

--- Applikationen/CoronaData/municipalityPositive.cs
+++ Applikationen/CoronaData/municipalityPositive.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,76 +10,38 @@ namespace Applikationen.CoronaData
 {
     public class municipalityPositive
     {
+        public string Municipality { get; set; }
+
         public double Positive { get; set; }
 
-        // For processing Region_summary.csv data
-        public municipalityPositive(double positive)
+        public double Tested { get; set; }
+
+        // For the Municipality_test_pos.csv data constructor
+        public municipalityPositive(string municipality, double tested, double positive)
         {
+            Municipality = municipality;
             Positive = positive;
+            Tested = tested;
         }
 
-        // For loading Region_summary.csv data
+        // For loading Municipality_test_pos.csv data
         public static IEnumerable<municipalityPositive> ReadCSV(string fileName)
         {
             List<municipalityPositive> listCSV = new List<municipalityPositive>();
             // We try to open a file and check whether it is a csv
             string lines = File.ReadAllText(fileName);
 
-            // We split the csv data at each semicolon to have separate fields/rows
-            string[] data = lines.Split(new[] { ";", "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            // We split the csv data at each break line to make all rows be a single index in a array to split it later into more easier to deal with components
+            string[] dataLines = lines.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 
-            var datalist = data.ToList();
-            datalist.RemoveRange(0, 5);
-            data = datalist.ToArray();
-
-            for (int i = 0; i < data.Length; i++)
+            // We split the lines into fields and add them to a list.
+            for (int i = 1; i < dataLines.Length-1; i++)
             {
-                data[i] = data[i].Trim();
-                data[i] = data[i].Replace(".", "");
+                string[] data = dataLines[i].Split(';');
+                listCSV.Add(new municipalityPositive(data[1], double.Parse(data[2]), double.Parse(data[3])));
             }
 
-
-            //for (int i = 0; i < 35;)
-            //{
-            //    listCSV.Add(new municipalityPositive(data[i], double.Parse(data[i + 1], System.Globalization.CultureInfo.InvariantCulture), double.Parse(data[i + 2], System.Globalization.CultureInfo.InvariantCulture), double.Parse(data[i + 3], System.Globalization.CultureInfo.InvariantCulture), double.Parse(data[i + 4], System.Globalization.CultureInfo.InvariantCulture)));
-            //    i = i + 5;
-            //}
-            // We return CoronaData with the data in the following order: Region, Tested (Total values), Positive, Hospitalized, Deaths
             return listCSV;
-        }
-
-        // ----------------
-        // Helper functions
-        // ----------------
-
-        // Calculate percentages, meant for use with percentagePositive
-        public double PercentageOfData(double newData, double totalData)
-        {
-            // We take the newData and find which percentage of the totalData it is
-            double percentComplete = (double)Math.Round((double)(100 * newData) / totalData);
-            return percentComplete;
-        }
-
-        // For getting data for all of Denmark
-        public int SumOfData(int[] collectionOfData)
-        {
-            // Add up the collection data
-            int total = collectionOfData.Sum();
-            return total;
-        }
-
-        // For getting total data in time period
-        public int SumOfDataTimespan(int[] collectionOfData, int timespanDays)
-        {
-            // Turn the array around to get newest data first instead of oldest
-            Array.Reverse(collectionOfData);
-
-            // Select only the indexes from the timespan needed
-            int[] dataInTimespan = collectionOfData.Take(timespanDays).ToArray();
-
-            // Add up the timespan data
-            int total = dataInTimespan.Sum();
-            return total;
         }
     }
 }
