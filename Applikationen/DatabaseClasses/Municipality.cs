7cb23431bedf85979cfc154e0c1a498b225aa859
Natasha <natashacbr@outlook.com>
2021-01-12 12:17:55 +0100

    Added comments to Municipality.cs

--- Applikationen/DatabaseClasses/Municipality.cs
+++ Applikationen/DatabaseClasses/Municipality.cs
@@ -13,23 +13,27 @@ using System.Data.SqlClient;
 // municipality.GetMunicipality();
 // On page where municipalities need to be called
 
+// Whole section is made by Natasha
 namespace Applikationen.MunicipalityFunctions
 {
 	public class Municipality
 	{
+		// We define municipalities variables
 		public int M_ID { get; set; }
 		public string Name { get; set; }
 
+		// Method to get municipalities from database
 		public void GetMunicipality()
 		{
+			// We create a list to store the municipalities from the database in
 			List<object> municipalities = new List<object>();
 
-			
+			// We open the connection to the database
 			SqlConnection cnn;
 			cnn = new SqlConnection(@"Data Source= DATAMATIKERDATA;Initial Catalog = team1; User ID = t1login; Password =t1login12345");
 			cnn.Open();
 
-
+			// We create the command and execute it
 			SqlCommand command;
 			SqlDataAdapter adapter = new SqlDataAdapter();
 			String sql = "";
@@ -40,16 +44,19 @@ namespace Applikationen.MunicipalityFunctions
 
 			var dataReader = command.ExecuteReader();
 
+			// While it's reading the data we add it to a new object for each row of the Municipalities table
 			while (dataReader.Read())
 			{
 				municipalities.Add(new Municipality() { M_ID = Convert.ToInt32(dataReader.GetValue(0)), Name = Convert.ToString(dataReader.GetValue(1)) });
 			}
 
+			// We write the municipality objects to the debug console
 			foreach(Municipality municipality in municipalities)
             {
 				Debug.WriteLine(municipality.M_ID + " " + municipality.Name);
 			};
 
+			// We delete the command and close the connection
 			command.Dispose();
 			cnn.Close();
 		}
